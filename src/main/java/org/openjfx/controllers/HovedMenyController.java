afdddbf13ecdc108c154785b2c40737c5f40311f
bereket <bereket.king@gmail.com>
2019-04-16 23:59:07 +0200

    try second window

--- src/main/java/org/openjfx/controllers/HovedMenyController.java
+++ src/main/java/org/openjfx/controllers/HovedMenyController.java
@@ -2,15 +2,36 @@ package org.openjfx.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class HovedMenyController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HovedMenyController implements Initializable {
     @FXML
     private Button jobbSøkerButton;
 
     @FXML
-    protected void handleJobbSøkerButtonAction(ActionEvent event){
+    protected void handleJobbSøkerButtonAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("employment_registration_form.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        Scene scene = new Scene(root, 800, 700);
+        stage.setTitle("Jobbsøker");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
